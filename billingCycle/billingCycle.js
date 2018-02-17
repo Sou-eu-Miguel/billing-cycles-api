f90b4b48a55502fcdc8e5933186909f934efed51
I-am-Miguel <miguelwelligton@gmail.com>
2018-02-16 23:23:17 -0300

    Contador de ciclos de pagamento

--- billingCycle/billingCycle.js
+++ billingCycle/billingCycle.js
@@ -2,23 +2,23 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-	name:{type: String, require: true},
-	value:{type: Number,min: 0, require: true }
+	name:{type: String, required: true},
+	value:{type: Number,min: 0, required: [true, 'Informe o valor do crédito'] }
 })
 
 const debtSchema = new mongoose.Schema({
-	name:{type: String, require: true},
-	value:{type: Number,min: 0, require: true },
+	name:{type: String, required: true},
+	value:{type: Number,min: 0, required: [true, 'Informe o valor do débito'] },
 	status:{
-		type: String,require: true, uppercase: true,
+		type: String,required: true, uppercase: true,
 		enum: ['PAGO', 'PENDENTE', 'AGENDADO', 'CANCELADO']
 	}
 })
 
 const billingCycleSchema = new mongoose.Schema({
-	name:{type: String, require: true},
-	month:{type: Number, min: 1, max: 12, require: true },
-	year:{type: Number, min: 1970, max: 2100, require: true },
+	name:{type: String, required: true},
+	month:{type: Number, min: 1, max: 12, required: true },
+	year:{type: Number, min: 1970, max: 2100, required: true },
 	credits:[creditSchema],
 	debts:[debtSchema]
 })
