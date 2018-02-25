c1668030136f049d122a228d4f528f1ff6da7813
Welligton <miguelwelligton@gmail.com>
2018-02-25 11:05:31 -0300

    padronização em mensagens de erros

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,7 +1,34 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get','post','put','delete'])
 BillingCycle.updateOptions({new:true, runValidators:true})
+
+
+BillingCycle.after('post',sendErrorsOrNext).after('put',sendErrorsOrNext)
+
+
+function sendErrorsOrNext(req, res, next){
+	const bundle = res.locals.bundle
+
+	if(bundle.errors){
+
+		let erros = parseErros(bundle.errors)
+		res.status(500).json({erros})
+
+	}else{
+		next()
+	}
+}
+
+function parseErros(NodeRestErrors){
+	let errors = []
+	
+	_.forIn(NodeRestErrors, error => errors.push(error.message))
+
+	return errors
+}
+
 
 BillingCycle.route('count',function(req,res,next){
 	BillingCycle.count(function(error, value){
